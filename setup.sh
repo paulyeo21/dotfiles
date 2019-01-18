6289d4903ccf88b2636aaac544352a83e5800ee2
Paul Yeo <paulyeo21@gmail.com>
2019-01-18 17:37:35 -0500

    Change shell to zsh after downloading

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 install_homebrew() {
   if ! command -v brew >/dev/null; then
-    echo "Installing homebrew..."
     /usr/bin/ruby -e \
       "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
@@ -22,9 +21,18 @@ setup_dotfiles() {
   fi
   ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
   ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
-  source ~/.zshrc
   ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
   ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+}
+
+setup_zsh() {
+  local shell_path;
+  shell_path="$(command -v zsh)"
+
+  if ! grep "$shell_path" /etc/shells > /dev/null 2>&1 ; then
+    sudo sh -c "echo $shell_path >> /etc/shells"
+  fi
+  sudo chsh -s "$shell_path" "$USER"
 }
 
 setup_vundle() {
@@ -40,6 +48,8 @@ setup_tpm() {
 install_homebrew
 install_dependencies
 setup_dotfiles
+setup_zsh
 setup_vundle
 setup_tpm
 
+echo "Finished"
