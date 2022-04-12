eb04114286dec45624429662fee6fa7457bcd331
Kwangu Paul Yeo <paulyeo@amazon.com>
2022-04-12 21:41:37 +0000

    Add zsh changes for amz linux dev desktop

--- zsh/config/alias.zsh
+++ zsh/config/alias.zsh
@@ -8,3 +8,18 @@ alias cp="cp -r"
 alias ctags="/usr/local/bin/ctags"
 alias pip="pip3"
 alias python="python3"
+
+alias b=brazil
+alias bb=brazil-build
+
+alias bba='brazil-build apollo-pkg'
+alias bre='brazil-runtime-exec'
+alias brc='brazil-recursive-cmd'
+alias bws='brazil ws'
+alias bwsuse='bws use --gitMode -p'
+alias bwscreate='bws create -n'
+alias brc=brazil-recursive-cmd
+alias bbr='brc brazil-build'
+alias bball='brc --allPackages'
+alias bbb='brc --allPackages brazil-build'
+alias bbra='bbr apollo-pkg'
--- zsh/config/general.zsh
+++ zsh/config/general.zsh
@@ -1,3 +1,3 @@
-export PS1='%n@%1~$ ' # custom prompt: paulyeo@exodia$
+# export PS1='%n@%1~$ ' # custom prompt: paulyeo@exodia$
 export EDITOR="vim"
 export REACT_EDITOR='vim'
