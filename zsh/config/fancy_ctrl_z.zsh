c0d978327cfd1ab88776ae823e89f45d9d62a7eb
Paul Yeo <paulyeo21@gmail.com>
2024-07-15 17:43:49 -0700

    Add <C-z> back to vim functionality

---
+++ zsh/config/fancy_ctrl_z.zsh
@@ -0,0 +1,12 @@
+# <C-z> to go back to vim
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line
+  else
+    zle push-input
+    zle clear-screen
+  fi
+}
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
