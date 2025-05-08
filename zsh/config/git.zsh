1cead4950c377304d376fe20e214aaa6e658a56f
Paul Yeo <paulyeo21@gmail.com>
2025-05-08 16:23:10 -0700

    Add g prune

--- zsh/config/git.zsh
+++ zsh/config/git.zsh
@@ -1,13 +1,12 @@
-
 # No arguments: `git status`
 # With arguments: acts like `git`
 function g() {
-	if [[ $# > 0 ]]; then
+  if [[ "$1" == "pru" ]]; then
+    git fetch --prune && \
+    git branch --merged | grep -vE "^\*|main|master" | xargs -r git branch -d && \
+    git branch -vv | awk '/: gone]/{print $1}' | xargs -r git branch -D
+	elif [[ $# > 0 ]]; then
 		git $@
-  elif [[ "$1" == "prune" ]]; then
-    git fetch --prune && \                                                         # removes stale remote-tracking branches
-    git branch --merged | grep -vE "^\*|main|master" | xargs -r git branch -d && \ # lists local branches that are fully merged
-    git branch -vv | awk '/: gone]/{print $1}' | xargs -r git branch -D            # deletes local branches whose remotes are gone
 	else
 		git status
 	fi
