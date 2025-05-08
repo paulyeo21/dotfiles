d78b35462533adf372324ec9d1e941e5981c6981
Paul Yeo <paulyeo21@gmail.com>
2025-05-08 16:16:43 -0700

    WIP 5/8/25

--- zsh/config/alias.zsh
+++ zsh/config/alias.zsh
@@ -8,3 +8,12 @@ alias cp="cp -r"
 alias ctags="/usr/local/bin/ctags"
 alias pip="pip3"
 alias python="python3"
+
+alias fix-gov='(
+     echo "
+     rdr pass inet proto tcp from any to any port 80 -> 127.0.0.1 port 9080
+     rdr pass inet proto tcp from any to any port 443 -> 127.0.0.1 port 9443
+     " | sudo pfctl -ef -
+   ) && (
+       printf "rdr pass inet proto tcp from any to any port = 80 -> 127.0.0.1 port 9080\nrdr pass inet proto tcp from any to any port = 443 -> 127.0.0.1 port 9443\n" | sudo pfctl -ef -
+     )'
--- zsh/config/git.zsh
+++ zsh/config/git.zsh
@@ -4,6 +4,10 @@
 function g() {
 	if [[ $# > 0 ]]; then
 		git $@
+  elif [[ "$1" == "prune" ]]; then
+    git fetch --prune && \                                                         # removes stale remote-tracking branches
+    git branch --merged | grep -vE "^\*|main|master" | xargs -r git branch -d && \ # lists local branches that are fully merged
+    git branch -vv | awk '/: gone]/{print $1}' | xargs -r git branch -D            # deletes local branches whose remotes are gone
 	else
 		git status
 	fi
