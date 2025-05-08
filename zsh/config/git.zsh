ce15be236ca0f4f34b25e63c4c74cb9b7a8f5e25
Paul Yeo <paulyeo21@gmail.com>
2025-05-08 16:33:58 -0700

    Use git alias for git pru instead

--- zsh/config/git.zsh
+++ zsh/config/git.zsh
@@ -1,11 +1,7 @@
 # No arguments: `git status`
 # With arguments: acts like `git`
 function g() {
-  if [[ "$1" == "pru" ]]; then
-    git fetch --prune && \
-    git branch --merged | grep -vE "^\*|main|master" | xargs -r git branch -d && \
-    git branch -vv | awk '/: gone]/{print $1}' | xargs -r git branch -D
-	elif [[ $# > 0 ]]; then
+	if [[ $# > 0 ]]; then
 		git $@
 	else
 		git status
